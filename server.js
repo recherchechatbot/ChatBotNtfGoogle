80bb095ec1e994aa13f8626ebd643a9310b78fde
a.rousselot <a.rousselot@DT-130301>
2017-12-26 14:38:02 +0100

    f

--- server.js
+++ server.js
@@ -805,6 +805,7 @@ function processV1Request(request, response) {
             let responseToUser = {
                 googleRichResponse: googleRichResponse
             }
+            sendResponse(responseToUser);
         }
     };
     // If undefined or unknown action use the default handler
