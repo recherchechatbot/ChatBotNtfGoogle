8145e350e624fd2e9a1a5dd8fc2faf01617b0f1d
a.rousselot <a.rousselot@DT-130301>
2017-12-26 16:15:26 +0100

    f

--- server.js
+++ server.js
@@ -1024,25 +1024,25 @@ const app = new DialogflowApp();
 console.log("PAR ICIIIIIIIIIII: " + JSON.stringify(app.ask));
 const googleRichResponse = app.buildRichResponse()
     .addSimpleResponse('This is the first simple response for Google Assistant')
-    .addSuggestions(
-    ['Suggestion Chip', 'Another Suggestion Chip'])
-    // Create a basic card and add it to the rich response
-    .addBasicCard(app.buildBasicCard(`This is a basic card.  Text in a
- basic card can include "quotes" and most other unicode characters
- including emoji ??.  Basic cards also support some markdown
- formatting like *emphasis* or _italics_, **strong** or __bold__,
- and ***bold itallic*** or ___strong emphasis___ as well as other things
- like line  \nbreaks`) // Note the two spaces before '\n' required for a
-        // line break to be rendered in the card
-        .setSubtitle('This is a subtitle')
-        .setTitle('Title: this is a title')
-        .addButton('This is a button', 'https://assistant.google.com/')
-        .setImage('https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png',
-        'Image alternate text'))
-    .addSimpleResponse({
-        speech: 'This is another simple response',
-        displayText: 'This is the another simple response ??'
-    });
+ //   .addSuggestions(
+ //   ['Suggestion Chip', 'Another Suggestion Chip'])
+ //   // Create a basic card and add it to the rich response
+ //   .addBasicCard(app.buildBasicCard(`This is a basic card.  Text in a
+ //basic card can include "quotes" and most other unicode characters
+ //including emoji ??.  Basic cards also support some markdown
+ //formatting like *emphasis* or _italics_, **strong** or __bold__,
+ //and ***bold itallic*** or ___strong emphasis___ as well as other things
+ //like line  \nbreaks`) // Note the two spaces before '\n' required for a
+ //       // line break to be rendered in the card
+ //       .setSubtitle('This is a subtitle')
+ //       .setTitle('Title: this is a title')
+ //       .addButton('This is a button', 'https://assistant.google.com/')
+ //       .setImage('https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png',
+ //       'Image alternate text'))
+ //   .addSimpleResponse({
+ //       speech: 'This is another simple response',
+ //       displayText: 'This is the another simple response ??'
+ //   });
 console.log("mon google rich response generique tout pourri: " + JSON.stringify(googleRichResponse));
 // Rich responses for Slack and Facebook for v1 webhook requests
 const richResponsesV1 = {
