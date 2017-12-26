e662e188559cc9e722fd63b0e50ac2f4fefa43ab
a.rousselot <a.rousselot@DT-130301>
2017-12-26 14:59:38 +0100

    g

--- server.js
+++ server.js
@@ -913,7 +913,7 @@ function processV1Request(request, response) {
         console.log("on est dans le cas où on doit prononcer un favori");
         text = "j'ai trouvé " + arrayProductsFull[productIndex][0] + " de " + arrayProductsFull[productIndex][4] + " dans tes favoris, on reste là dessus?"
         }
-        //si le produit est en promo
+        //si le produit est en promo !
         else if (arrayProductsFull[productIndex][7] && arrayProductsFull[productIndex][7] !== null) {
             console.log("on est dans la partie promo");
             text = text + ': \n' + arrayProductsFull[productIndex][0] + ' ' + arrayProductsFull[productIndex][4] + " qui est en promotion à " + arrayProductsFull[productIndex][5] + " au lieu de " + (parseFloat(arrayProductsFull[productIndex][5].replace(",", ".")) + parseFloat(arrayProductsFull[productIndex][7].Label.replace(",", "."))).toFixed(2) + "€";
